0d6b79d5b3a1221db6320cd1d961e0c4d4360ceb
HriKosh <142148963+HriKosh@users.noreply.github.com>
2023-08-13 17:18:27 +0530

    re-wrote the entire main script so it uses axios and is smaller

--- index.js
+++ index.js
@@ -1,75 +1,26 @@
-const {
-    get
-} = require('https')
-const fetch = (msglimit, callback) => {
-    if (typeof (callback) === 'function') {
-        let url = "https://www.reddit.com/r/krunkerio.json?limit=100"
-        if (msglimit != undefined && msglimit != 0) {
-            if (typeof (msglimit) === 'number') {
-                let numbStr = msglimit.toString()
-                if (numbStr.includes('.')) {
-                    numbStr = numbStr.split('.')[0]
-                    url = "https://www.reddit.com/r/krunkerio.json?limit=" + numbStr
-                } else {
-                    url = "https://www.reddit.com/r/krunkerio.json?limit=" + numbStr
-                }
-            } else {
-                throw "KrunkMemes -> Invalid Parameter for Post Limit Received"
-            }
-        }
-        get(url, e => {
-            let resdata = []
-            e.on('data', chunk => {
-                resdata.push(chunk)
-            });
-            e.on('end', () => {
-                const res = JSON.parse(Buffer.concat(resdata))
-                let memes = []
-                let errors = []
-                const jsonMemeResponse = (data, url) => {
-                    return {
-                          author: data.data.author
-                        , image: url
-                        , upvoteRatio: data.data.upvote_ratio
-                        , upvotes: data.data.ups
-                        , downvotes: data.data.downs
-                        , postedTime: data.data.created_utc
-                        , redditURL: data.data.permalink
-                        , postTitle: data.data.title
-                    }
-                }
-                let data = res.data.children
-                for (let i = 0; i < data.length + 1; i++) {
-                    if (i === data.length) {
-                        const return_obj = {
-                              data: memes
-                            , errors: errors
-                        }
-                        callback(return_obj)
-                    } else {
-                        if (data[i].data.link_flair_text.toLowerCase() === "meme" && data[i].data.is_video != true) {
-                            let img = data[i].data.url
-                            if (img.endsWith('.png') || img.endsWith('.jpg') || img.endsWith('.webp')) {
-                                memes.push(jsonMemeResponse(data[i], img))
-                            } else {
-                                let thumb = data[i].data.thumbnail
-                                if (thumb.endsWith('.png') || thumb.endsWith('.jpg') || thumb.endsWith('.webp')) {
-                                    memes.push(jsonMemeResponse(data[i], thumb))
-                                } else {
-                                    errors.push(data[i].data.permalink)
-                                }
-                            }
-                        }
-                    }
-                }
-            })
-        }).on("error", (err) => {
-            throw "KrunkMemes -> Error While Fetching Memes -> " + err
-        })
-    } else {
-        throw "KrunkMemes -> Invalid Parameter for Callback Function Received"
+//install this package without running
+//TODO: make it so that when installing this api it installs axios as well
+const axios = require('axios/dist/node/axios.cjs');
+
+// meme getter
+async function fetchMemesFromSubreddit(subreddit) {
+    try {
+        //fetch shit
+        const response = await axios.get(`https://www.reddit.com/r/${subreddit}/top.json?limit=10&t`);
+        const memes = response.data.data.children;
+
+        // process/display da funny pics
+        memes.forEach((meme, index) => {
+            const memeData = meme.data;
+            console.log(`Meme ${index + 1}: ${memeData.title}`);
+            console.log(`URL: ${memeData.url}`);
+            console.log('---');
+        });
+    } catch (error) {
+        //try catch my love
+        console.error('Error fetching memes:', error);
     }
 }
-module.exports.get = (limit, callback) => {
-    fetch(limit, callback)
-}
+
+//call it with 'KrunkerIO' also works with other subreddits
+fetchMemesFromSubreddit('KrunkerIO');
